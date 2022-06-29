726a11ebde376b96e3c1ddf08e071647c80bd44e
La Min Ko Ko <101461436+laminkoko243@users.noreply.github.com>
2022-06-29 22:34:39 +0630

    Initial Upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// // Selecting elements by DOM
+// const navLink = document.querySelector("#navLinks")
+// const closeIconElmnt = document.querySelector("#closeIcon")
+// const menuIconElmnt = document.querySelector("#menuIcon")
+
+// // Functions
+// const showMenu = function() {
+//     navLink.style.right = "0";
+// }
+// const hideMenu = function() {
+//     navLink.style.right = "-200px";
+// }
+
+// menuIconElmnt.addEventListener("click", showMenu);
+// closeIconElmnt.addEventListener("click", hideMenu);
